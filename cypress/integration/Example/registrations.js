88b0a7cd46249bc7ffc6d3ce309b5b38fa00236c
Arif <alyaref555@gmail.com>
2021-11-19 10:33:27 +0500

    Registration update

---
+++ cypress/integration/Example/registrations.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress"/> 
+ import registration from "../../support/pageobjects/registration"
+
+describe('Test Suite for JpetStore', function() {
+
+    it('First test case' ,  function () {
+        const reg = new registration();
+         cy.visit('https://petstore.octoperf.com/actions/Account.action?signonForm=')
+         reg.getCatalog().click()
+          reg.getForm().click()
+          reg.getUserName().type('10500')
+         reg.getPassword().type('Nothing123')
+          reg.getConfirmPass().ype('Nothing123')
+          reg.getFirstName().type('Kashif')
+          reg.getLastName().type('Ali')
+          reg.getEmail().type('kashif4545@gmail.com')
+          reg.getPhoneNumber().type('03369167046')
+          reg.getAddress().type('Rawalpindi Punjab')
+          reg.getCity1().type('Chitral, Kpk')
+          reg.getCity().type('Rawalpindi')
+          reg.getprovince().type('Punjab')
+          reg.getCode2().type('8050')
+          reg.pak().type('Pak')
+          
+        })
+        
+
+})
---
+++ cypress/support/pageobjects/registration.js
@@ -0,0 +1,65 @@
+class registration {
+
+    getCatalog()
+    {
+     return cy.get('#Catalog > a')
+    }
+    getForm()
+    {
+       return cy.get('#Catalog > form')
+    }
+    getUserName()
+    {
+     return cy.get(':nth-child(2) > tbody > :nth-child(1) > :nth-child(2)')
+    }
+    getPassword()
+    {
+       return cy.get(':nth-child(2) > tbody > :nth-child(2) > :nth-child(2) > input')
+    }
+    getConfirmPass()
+    {
+     return cy.get(':nth-child(2) > tbody > :nth-child(3) > :nth-child(2) > input')
+    }
+    getFirstName()
+    {
+     return cy.get(':nth-child(4) > tbody > :nth-child(1) > :nth-child(2) > input')
+    }
+    getLastName()
+    {
+       return cy.get(':nth-child(4) > tbody > :nth-child(2) > :nth-child(2) > input')
+    }
+    getEmail()
+    {
+       return cy.get(':nth-child(4) > tbody > :nth-child(3) > :nth-child(2) > input')
+    }
+    getPhoneNumber()
+    {
+       return cy.get(':nth-child(4) > tbody > :nth-child(4) > :nth-child(2) > input')
+    }
+    getAddress()
+    {
+       return cy.get(':nth-child(5) > :nth-child(2) > input')
+    }
+    getCity()
+    {
+       return cy.get(':nth-child(6) > :nth-child(2) > input')
+    }
+   getCity1()
+   {
+   return cy.get(':nth-child(7) > :nth-child(2) > input')
+   }
+   getprovince()
+   { 
+   return cy.get(':nth-child(8) > :nth-child(2) > input')
+   }
+   getCode2()
+   {
+   return cy.get(':nth-child(9) > :nth-child(2) > input')
+   }
+  getCountry()
+  {
+   return cy.get(':nth-child(10) > :nth-child(2) > input')
+  }
+ 
+}
+export default registration
